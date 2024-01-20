5bcc0c2e202f15a9ef2c6099935cd30fd69d9c30
Lucas Santos <lucas.santos@sp.senai.br>
2024-01-19 21:49:21 -0300

    Adicionando código para calcular o valor final do produto

--- Program.cs
+++ Program.cs
@@ -10,6 +10,48 @@ namespace CalculaDescontos
     {
         static void Main(string[] args)
         {
+            const string SENHA_PADRAO = "senha";
+
+            Console.WriteLine("Bem vindo ao DescontosApp");
+
+            // Console.WriteLine("Por favor, digite a senha:");
+            //string senha = Console.ReadLine();
+
+            if ("senha" != SENHA_PADRAO)
+            {
+                Console.WriteLine("A senha está incorreta!");
+                Console.WriteLine("Um bjo.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            // Solicita o valor do produto
+            Console.WriteLine("Qual o valor do produto?");
+            double valorProduto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a porcentagem de desconto?");
+            double porcentagemDesconto = double.Parse(Console.ReadLine());
+
+            // Verifica se as taxas de desconto estão corretas
+            if (porcentagemDesconto > 50 || porcentagemDesconto < 0)
+                MostraMensagemErro("A porcentagem não pode ser maior que 50% ou menor que 0%");
+            else if (valorProduto < 200 && porcentagemDesconto > 20)
+                MostraMensagemErro("Apenas descontos menores que 20% são permitidos para compras abaixo de 200 reais!");
+            else if (valorProduto < 100 && porcentagemDesconto > 10)
+                MostraMensagemErro("Apenas descontos menores que 10% são permitidos para compras abaixo de 100 reais!");
+
+            double valorFinal = valorProduto - (valorProduto * (porcentagemDesconto / 100));
+
+            Console.Write("O valor final do produto é ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(valorFinal.ToString("C"));
+        }
+
+        static void MostraMensagemErro(string Mensagem = "Erro na execução")
+        {
+            Console.WriteLine(Mensagem);
+            Console.ReadKey();
+            Environment.Exit(0);
         }
     }
 }
